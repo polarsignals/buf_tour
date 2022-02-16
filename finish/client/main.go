9fa6d01a251e6381544d38f13f746590fad13bc3
Thor Hansen <me@thor-hansen.com>
2022-02-16 09:44:56 -0600

    http get request of pet

--- finish/client/main.go
+++ finish/client/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	// This import path is based on the name declaration in the go.mod,
 	// and the gen/proto/go output location in the buf.gen.yaml.
@@ -34,6 +37,25 @@ func run() error {
 	}
 
 	log.Println("Successfully PutPet")
-	fmt.Println(resp.Pet)
+
+	request := fmt.Sprintf("http://127.0.0.1:8080/v1/pets/%v", resp.Pet.PetId)
+	log.Println("HTTP GET request", request)
+	r, err := http.Get(request)
+	if err != nil {
+		return fmt.Errorf("failed to Get pet: %w", err)
+	}
+	defer r.Body.Close()
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response: %w", err)
+	}
+
+	petresp := &petv1.GetPetResponse{}
+	if err := json.Unmarshal(b, petresp); err != nil {
+		return fmt.Errorf("failed to unmarshal pet: %w", err)
+	}
+
+	fmt.Println("HTTP Get request returned: ", petresp)
 	return nil
 }
